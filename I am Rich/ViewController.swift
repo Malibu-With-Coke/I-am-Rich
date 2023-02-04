8312edf7a88ffd0d2a9208c08ade8a1b023eeacd
JungGyeom <love@handong.ac.kr>
2023-02-04 12:15:00 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by MCNL on 2023/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
